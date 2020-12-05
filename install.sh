e18b50b19607b2d83b0eec210603b7eb25ea5977
FrostyGekyume <75546030+FrostyGekyume@users.noreply.github.com>
2020-12-05 19:49:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,1 @@
-apt update
-apt upgrade
-pkg install wget
-pkg install ffmpeg
-pkg install nodejs
-pkg install tesseract
-npm i cwebp
-npm i ytdl
-npm i node-tesseract-ocr
-npm i
-node index.js
+apt update && apt upgrade && pkg install wget && pkg install ffmpeg && pkg install nodejs && pkg install tesseract && npm i cwebp && npm i ytdl && npm i node-tesseract-ocr && npm i && node index.js
